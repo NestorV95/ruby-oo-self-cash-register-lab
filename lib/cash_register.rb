7822ab51920d1ee8c1237c3e4d44363b1863a9da
Nestor <nestor.venegas95@gmail.com>
2021-02-24 21:34:48 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
 
+class CashRegister
+    attr_reader :title, :discount, :price
+    
+    @@items=[]
+    
+    def initialize(discount=0)
+       @total = 0
+       @discount = discount
+    end
+    def total
+       @total
+    end
+    
+    def add_item(title, price, quantity=1)
+       @title = title
+       @price = price
+       @total += @price
+       
+    end
+
+    def apply_discount
+
+    end
+     
+    def self.items
+      @@items
+    end
+
+    def void_last_transaction
+
+    end
+
+
+end
