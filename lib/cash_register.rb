c4f8ee9f72d821a959bca41bfde812bc468c5265
Nestor <nestor.venegas95@gmail.com>
2021-02-25 12:32:18 -0600

    commit tree 2f7b926b64e7b46126960130237054451b5f674d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,35 +1,41 @@
-
+require "pry"
 class CashRegister
-    attr_reader :title, :discount, :price
-    
-    @@items=[]
-    
+    attr_accessor :total
+    attr_reader :title, :price, :discount
     def initialize(discount=0)
        @total = 0
        @discount = discount
-    end
-    def total
-       @total
-    end
-    
+       @items=[]
+    end 
+   
     def add_item(title, price, quantity=1)
-       @title = title
-       @price = price
-       @total += @price
-       
-    end
-
-    def apply_discount
-
+        @total += (price * quantity)
+        @last = (price * quantity)
+        until quantity == 0
+        @items << title
+        quantity -= 1
+        end
     end
      
-    def self.items
-      @@items
+    def apply_discount
+        if @discount==0
+          "There is no discount to apply." 
+        else 
+           @total -= (0.01*@discount * @total).to_i
+           "After the discount, the total comes to $#{@total}."
+        end
+        
+    end
+
+    def items
+      @items
     end
 
     def void_last_transaction
-
+      self.total -= @last
+      if total == 0.0
+        "returns the total to 0.0 if all items have been removed"
+      end
     end
-
 
 end
